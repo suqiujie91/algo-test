cbbf5731108a01189eb93f44921cf94affba86c1
suqj <suqj@amegroups.com>
2019-11-12 02:32:33 +0800

    添加数据结构-数组实现 2

---
+++ Stru/Arr/test.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '..'. DIRECTORY_SEPARATOR .'autoload.php';
+
+use stru\arr\ArrayList;
+
+$arr = new ArrayList(10);
+$arr->init();
+$arr->insert(9,11);
+$arr->delete(8);
+$arr->delete(9);
+$arr->delete(6);
+var_dump($arr->data);
+//$data = $arr->init();
